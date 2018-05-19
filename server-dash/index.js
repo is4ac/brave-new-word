a21409f7fa8a4a6d89f8d546aaabad50de8a9e19
Vishesh Kumar <vishesh@visheshk.net>
2018-05-19 16:53:21 -0400

    Cleaning up old code

--- server-dash/index.js
+++ server-dash/index.js
@@ -8,7 +8,6 @@ const assert = require('assert');
 
 app.use(express.static('public/'));
 
-
 // const db = require('./config/db');
 // var ObjectID = require('mongodb').ObjectID
 
@@ -24,28 +23,6 @@ var mongojs = require('mongojs');
 
 var db = mongojs("mongodb://localhost:27017/wordflood");
 
-/*
-var blessed = require('blessed')
-    // , contrib = require('../server-dash/blessed-deps');
-var contrib = require('blessed-contrib');
-
-var screen = blessed.screen();
-
-var grid = new contrib.grid({rows: 16, cols: 16, screen: screen})
-
-var log = grid.set(0, 0, 6, 6, contrib.log, 
-  { fg: "green"
-  , selectedFg: "green"
-  , label: 'Submitted Words'})
-
-var table =  grid.set(0, 7, 8, 5, contrib.table, 
-  { keys: true
-  , fg: 'green'
-  , label: 'Active Users'
-  , columnSpacing: 1
-  , columnWidth: [38, 10]}
-)
-*/
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -62,21 +39,10 @@ var ref = fdb.ref("WFLogs_V1_0_2");
 ref.on("child_added", function(snapshot, prevChildKey) {  
   var newPost = snapshot.val();
   var postPayload = newPost.payload;
-  // console.log("Author: " + newPost.author);
-  // console.log("Title: " + newPost.title);
-  // console.log("Previous Post ID: " + prevChildKey);
-  // console.log(newPost);
-  // io.sockets.emit("newWord", postPayload);
   if (newPost.key == "WF_Submit") {
-    // console.log("Word " + newPost.payload.word + " success: " + newPost.payload.success + " Points: " + newPost.payload.scoreTotal);
- 
-    // log.log("Word " + newPost.payload.word + " success: " + newPost.payload.success + " Points: " + newPost.payload.scoreTotal);
-    // io.emit('new-logs', newPost);
-    // console.log();
   }
 
   else if (newPost.key == "WF_GameState") {
-    // updateUserList(newPost);
     if (newPost.hasOwnProperty("username")) {
       // console.log(newPost["username"]);
       updateUserList(newPost);
@@ -134,150 +100,12 @@ setUsers = function () {
   latest.toArray(function (err, res) {
     if (err){ return err; }
     else {
-      console.log("new user");
-      console.log(res);
+      // console.log("new user");
+      // console.log(res);
       io.sockets.emit("setScoreList", res);
     }
   });
 }
-
-
-/*
-function populateSubmitLog() {
-  db.allLogs.find(
-    {"key": "WF_Submit"},
-    {"username": 1,
-    "payload": 1}
-  ).limit(5).sort({"timestampEpoch": -1}, function (err, docs) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      for (i = 0; i < 5; i++){
-        doc = docs[i];
-        
-        if (doc.payload != undefined){
-          // console.log(doc);
-          log.log("Word " + doc.payload.word + " success: " + doc.payload.success + " Points: " + doc.payload.scoreTotal);
-        }  
-      }
-      
-    }
-  });
-  
-}
-*/
-/*
-function refreshUserList() {
-  db.allLogs.distinct(
-     "username",
-     {}, // query object
-     function(err, docs){
-      if (err) {}
-      else {
-        // console.log(typeof(docs) + "   tt.   ");
-        // console.log(docs[0]);
-        // userlist.push(docs);
-        refreshUserTable(docs);
-      }
-     }
-  );
-}
-*/
-
-/*
-function getScore (username, callback) {
-  db.allLogs.find(
-    {$and: [{"key": "WF_GameState"},
-            {"username": username}]}
-  ).limit(1).sort({"timestampEpoch": -1}, score = function (err, docs) {
-  // db.allLogs.findOne({$and: [{"key": "WF_GameState"}, {"username": username}]}, {$orderBy: {"timestampEpoch": -1}}, function (err, res) {
-    if (err) {console.log(err); callback(-5);}
-    else {
-      if (docs.length > 0) {
-        res = docs[0];
-        if (res.payload != undefined){
-          // console.log(res.payload.totalScore);
-          score = res.payload.totalScore;
-          callback(score);
-        }
-        else {
-          // console.log(res);
-          // score = 0;
-          callback(-1);
-        }
-      }
-    }
-  });
-
-  // return score;
-}
-
-function refreshUserTable(userlist) {
-  var data = [];
-  // var userlist = [];
-
-  // console.log(userlist);
-  if(userlist != undefined){
-    // console.log(userlist.length);
-    for (var i = 0; i < userlist.length; i++) {
-      var row = [];         
-      row.push(userlist[i]);
-      
-      score = 0;
-      score = getScore(userlist[i], function (res) {
-        // console.log(res);
-        // row.push(res);
-        // score = res;
-        return res;
-      });
-      // console.log(score);
-      row.push(0);
-      db.userConnects
-      // row.push()
-      // row.push()
-      data.push(row);
-      // console.log(data);
-    }
-  }
-  // console.log(data);
-  
-  table.setData({headers: ['Username', 'Score'], data: data})
-}
-*/
-
-/*
-table.focus();
-
-table.on('select',function(node){
-  if (node.myCustomProperty){
-    console.log(node.myCustomProperty);
-  }
-  console.log(node.name);
-});
-
-setInterval(function () {
-  refreshUserList();
-  screen.render();
-}, 2000);
-populateSubmitLog();
-
-updateUserList = function (newPost) {
-
-  db.userConnects.update(
-    {"username": newPost.username},
-    {$set: {
-      "lastActionEpoch": newPost.timestampEpoch,
-      "lastActionTime": newPost.timestamp
-    } },
-    {upsert: true},
-    function(err, docs) {}
-  );
-  refreshUserTable();    
-}
-
-*/
-
 
 app.get('/', function(req, res){
   // res.send('<h1>Hello world</h1>');
@@ -311,6 +139,11 @@ io.on('connection', function(socket){
     
   });
 
+  socket.on('getUserMoves', function (msg) {
+    console.log("maker user scores: " + msg);
+    
+  })
+
   socket.on('disconnect', function(){
     // console.log('user disconnected');
   });
@@ -319,17 +152,3 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
-/*
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-  return process.exit(0);
-});
-
-
-screen.on('resize', function() {
-  table.emit('attach');
-  log.emit('attach');
-});
-
-screen.render()
-*/
